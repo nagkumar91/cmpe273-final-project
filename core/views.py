00e5986b4d8f8335cce9c7301310b9adf5a5b0db
varsha Jayadev <varshaj.vj@gmail.com>
2015-05-06 18:33:02 -0700

    saving access tokens in our model

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 from django.template import RequestContext
-
+from social.apps.django_app.default.models import UserSocialAuth
 
 def homepage(request):
     return render_to_response("home.html",
@@ -19,4 +19,12 @@ def login_error(request):
 @login_required()
 def logged_in_page(request):
     print request.user
+    for a in UserSocialAuth.objects.all():
+        if a.user == request.user:
+            request.user.user_access_token = a.extra_data.get("access_token").get("oauth_token")
+            request.user.user_access_secret = a.extra_data.get("access_token").get("oauth_token_secret")
+            request.user.save()
+
+
+
     return render_to_response("logged_in.html", {'user': request.user}, context_instance=RequestContext(request))
