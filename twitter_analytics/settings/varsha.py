71e261c232bceb47907ad8d38330ced1b470db51
varsha Jayadev <varshaj.vj@gmail.com>
2015-05-07 19:39:29 -0700

    added form to take user's email ID

--- twitter_analytics/settings/varsha.py
+++ twitter_analytics/settings/varsha.py
@@ -24,8 +24,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twitter_analytics',
+        'USER': 'varsha',
+        'PASSWORD': 'varsha',
+        'HOST': '',
+        'PORT': '',
     }
 }
 ########## END DATABASE CONFIGURATION
