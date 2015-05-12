e74878ff832d5b76b1896b68ecb6510965c84126
Nagkumar <me@nagkumar.com>
2015-05-11 23:24:52 -0700

    Beautified the page with the list of positive and negative tweets

--- core/views.py
+++ core/views.py
@@ -107,19 +107,17 @@ def older_result_detail(request, id):
 
 
 @login_required()
-def older_result_detail_with_hash(request, result_id, hash_tag):
+def older_result_detail_with_hash(request, result_id):
     try:
         analysis_result = HashTagAnalysisResult.objects.get(pk=result_id)
         positive_tweets = analysis_result.positive_tweets.all()
         negative_tweets = analysis_result.negative_tweets.all()
-        neutral_tweets = analysis_result.neutral_tweets.all()
 
         return render_to_response("detailed_analysis_with_tweets.html",
             {
                 'result': analysis_result,
                 'positive_tweets': positive_tweets,
-                'negative_tweets': negative_tweets,
-                'neutral_tweets': neutral_tweets
+                'negative_tweets': negative_tweets
             })
     except ObjectDoesNotExist:
         return HttpResponseNotFound()
